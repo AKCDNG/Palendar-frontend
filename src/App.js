4d38a023f4be32e4d869cdb7f119c6ee85bebbd0
Anish <anishkakaiya@outlook.com>
2022-07-16 22:50:36 +0100

    literally just fixing and linting everything

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/login';
 import Signup from './components/signup';
 import PersonalCalendar from './components/personalCalendar';
 import GroupCalendar from './components/groupCalendar';
-import FirstPage from './components/firstPage';
 
 function App() {
   return (
